ffa1b7a3c9d0e869e3d81c31903bccd7ba096bbd
Kenul Perera <72089275+KLP-codebase@users.noreply.github.com>
2026-07-02 13:41:22 +0530

    Add DoctorRepository interface for Doctor model

--- backend/src/main/java/com/smartcare/backend/repository/DoctorRepository.java
+++ backend/src/main/java/com/smartcare/backend/repository/DoctorRepository.java
@@ -4,6 +4,8 @@ import com.smartcare.backend.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 }
